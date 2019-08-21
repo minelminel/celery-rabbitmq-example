eb9b6fcd2def9290bf583344c14da062caf645b3
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-08-21 02:00:40 -0400

    minor formatting fixes

--- src/artifice/scraper/foreground/models/base.py
+++ src/artifice/scraper/foreground/models/base.py
@@ -4,6 +4,6 @@ from .. import db
 
 class BaseModel(db.Model):
     __abstract__ = True
-    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    id =            db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
     created_at =    db.Column(db.DateTime(), nullable=False, default=datetime.datetime.utcnow())
     modified_at =   db.Column(db.DateTime(),nullable=True, onupdate=datetime.datetime.utcnow())
--- src/artifice/scraper/foreground/models/queue.py
+++ src/artifice/scraper/foreground/models/queue.py
@@ -3,4 +3,4 @@ from .base import BaseModel, db
 class Queue(BaseModel):
     __tablename__ = 'queue'
     url =           db.Column(db.String(500), nullable=False, unique=True)
-    status =     db.Column(db.String(10), nullable=False, default='READY')
+    status =        db.Column(db.String(10), nullable=False, default='READY')
