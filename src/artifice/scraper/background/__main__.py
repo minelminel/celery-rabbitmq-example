f9076ec0e97004f7709230ca6980ab0f14c38240
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-08-28 09:25:42 -0400

    allow background module to pull concurrency variable from settings file

--- src/artifice/scraper/background/__main__.py
+++ src/artifice/scraper/background/__main__.py
@@ -1,4 +1,4 @@
 from . import celery_app
 import artifice.scraper.config.settings as settings
 
-celery_app.start(argv=['celery', 'worker', '--loglevel', settings.CELERY_LOG_LEVEL, '--logfile', settings.CELERY_LOG_FILE])
+celery_app.start(argv=['celery', 'worker', '--concurrency={}'.format(settings.CELERY_WORKERS), '--loglevel', settings.CELERY_LOG_LEVEL, '--logfile', settings.CELERY_LOG_FILE])
