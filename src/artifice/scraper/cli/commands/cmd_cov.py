17e11ae391675df719665fd85cf64f70898d4bc4
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-08-28 09:38:47 -0400

    updating cli documentation strings

--- src/artifice/scraper/cli/commands/cmd_cov.py
+++ src/artifice/scraper/cli/commands/cmd_cov.py
@@ -8,9 +8,6 @@ import click
 def cli(path):
     """
     Run a test coverage report.
-
-    :param path: Test coverage path
-    :return: Subprocess call result
     """
     cmd = 'py.test --cov-report term-missing --cov {0}'.format(path)
     return subprocess.call(cmd, shell=True)
--- tests/scraper/test_foreground_resources_content.py
+++ tests/scraper/test_foreground_resources_content.py
@@ -6,3 +6,5 @@ from . import post_json, de_json
 # def test_foreground_resources_content_get(client):
 #     rv = client.get('/content')
 #     assert rv.status_code == 200
+#     assert 'msg' in rv.json.keys()
+#     assert 'reply' in rv.json.keys()
